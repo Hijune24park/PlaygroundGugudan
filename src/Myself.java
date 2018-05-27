8b64c97384f9250e66503829453cf3f3f0d0b4f9
Hyeonjune Park <Hyeonjune Park@DESKTOP-BE41DV1>
2018-05-27 18:59:05 +0900

    4,5단 변수, 값 입력 연습

--- src/Myself.java
+++ src/Myself.java
@@ -1,28 +1,49 @@
+import java.util.Scanner;
 
 public class Myself {
 	public static void main(String[] args) {
-		// 2단
-		System.out.println("2단");
-		System.out.println("2 * 1 = " + 2 * 1);
-		System.out.println("2 * 2 = " + 2 * 2);
-		System.out.println("2 * 3 = " + 2 * 3);
-		System.out.println("2 * 4 = " + 2 * 4);
-		System.out.println("2 * 5 = " + 2 * 5);
-		System.out.println("2 * 6 = " + 2 * 6);
-		System.out.println("2 * 7 = " + 2 * 7);
-		System.out.println("2 * 8 = " + 2 * 8);
-		System.out.println("2 * 9 = " + 2 * 9);
-		System.out.println();
-		// 3단
-		System.out.println("3단");
-		System.out.println("3 * 1 = " + 3 * 1);
-		System.out.println("3 * 2 = " + 3 * 2);
-		System.out.println("3 * 3 = " + 3 * 3);
-		System.out.println("3 * 4 = " + 3 * 4);
-		System.out.println("3 * 5 = " + 3 * 5);
-		System.out.println("3 * 6 = " + 3 * 6);
-		System.out.println("3 * 7 = " + 3 * 7);
-		System.out.println("3 * 8 = " + 3 * 8);
-		System.out.println("3 * 9 = " + 3 * 9);
+
+		int result = 4 * 1;
+		System.out.println(result);
+		
+		result = 4 * 2;	// int result = 4 *2;이라고 하면 안 된다. 똑같은 변수를 한 번 더 선언하는 꼴이 된다.
+		// 그냥 변수명만 입력하면 된다.
+		System.out.println(result);
+		
+		result = 4 * 3;	
+		System.out.println(result);
+		
+		result = 4 * 4;	
+		System.out.println(result);
+		
+		result = 4 * 5;	
+		System.out.println(result);
+		
+		result = 4 * 6;	
+		System.out.println(result);
+		
+		result = 4 * 7;	
+		System.out.println(result);
+		
+		result = 4 * 8;	
+		System.out.println(result);
+		
+		result = 4 * 9;	
+		System.out.println(result);
+		
+		System.out.println("출력한 단을 입력하시오: ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt(); 
+		
+		System.out.println(number+"단");
+		System.out.println(number + " * 1 =" + number * 1);
+		System.out.println(number + " * 2 =" + number * 2);
+		System.out.println(number + " * 3 =" + number * 3);
+		System.out.println(number + " * 4 =" + number * 4);
+		System.out.println(number + " * 5 =" + number * 5);
+		System.out.println(number + " * 6 =" + number * 6);
+		System.out.println(number + " * 7 =" + number * 7);
+		System.out.println(number + " * 8 =" + number * 8);
+		System.out.println(number + " * 9 =" + number * 9);
 	}
 }
